8679ee61e195d47ac73f26d7a35fe400a02ae28a
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2009-09-07 13:27:45 +0200

    Adapting to the new gsc 0.9.0 version
    Removed all provider less provider-words and removed the
    configuration dialog

--- src/docwordscompletion-plugin.c
+++ src/docwordscompletion-plugin.c
@@ -28,16 +28,9 @@
 #include <glib/gi18n-lib.h>
 #include <gedit/gedit-debug.h>
 #include <gconf/gconf-client.h>
-#include <gtksourcecompletion/gsc-trigger.h>
-#include <gtksourcecompletion/gsc-trigger-customkey.h>
-#include <gtksourcecompletion/gsc-trigger-userrequest.h>
-#include <gtksourcecompletion/gsc-trigger-autowords.h>
-#include "gsc-documentwords-provider.h"
+#include <gtksourcecompletion/gsc-completion.h>
+#include "gsc-provider-words.h"
 
-#include "gsc-geditopendoc-provider.h"
-#include "gsc-geditrecent-provider.h"
-
-#define OPEN_DOCS_TRIGGER_NAME "Open Documents Trigger"
 #define WINDOW_DATA_KEY	"DocwordscompletionPluginWindowData"
 
 #define GCONF_BASE_KEY "/apps/gedit-2/plugins/docwordscompletion"
@@ -167,6 +160,21 @@ docwordscompletion_plugin_finalize (GObject *object)
 	G_OBJECT_CLASS (docwordscompletion_plugin_parent_class)->finalize (object);
 }
 
+static void
+tab_added_cb (GeditWindow *geditwindow,
+              GeditTab    *tab,
+              gpointer     user_data)
+{
+        GeditView *view = gedit_tab_get_view (tab);
+        GscCompletion *comp = gsc_completion_new (GTK_TEXT_VIEW (view));
+        g_debug ("Adding Words provider");
+        GscProviderWords *dw  = gsc_provider_words_new();
+        gsc_completion_add_provider(comp,GSC_PROVIDER(dw), NULL);
+	
+        g_object_unref(dw);
+        g_debug ("provider registered");
+}
+
 
 static void
 impl_activate (GeditPlugin *plugin,
@@ -175,6 +183,11 @@ impl_activate (GeditPlugin *plugin,
 	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)plugin;
 	dw_plugin->priv->gedit_window = window;
 	gedit_debug (DEBUG_PLUGINS);
+
+	g_signal_connect (window, "tab-added",
+                          G_CALLBACK (tab_added_cb),
+                          NULL);
+
 
 }
 
@@ -190,489 +203,12 @@ impl_update_ui (GeditPlugin *plugin,
 		GeditWindow *window)
 {
 	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)plugin;
-	GscTrigger *od_trigger, *ur_trigger, *ac_trigger;
 	dw_plugin->priv->gedit_window = window;
 	gedit_debug (DEBUG_PLUGINS);
-	GtkTextView* view = GTK_TEXT_VIEW(gedit_window_get_active_view(window));
-	if (view!=NULL)
-	{
-		GscCompletion *comp = gsc_completion_get_from_view(view);
-		if (comp==NULL)
-		{
-			comp = GSC_COMPLETION (gsc_completion_new(GTK_TEXT_VIEW(view)));
-			GValue value = {0,};
-			g_value_init(&value,G_TYPE_STRING);
-			g_value_set_string(&value,dw_plugin->priv->conf->si_keys);
-			g_object_set (comp,
-				      "info-keys",
-				      dw_plugin->priv->conf->si_keys,
-				      NULL);
-		}
 
-		od_trigger = gsc_completion_get_trigger(comp, OPEN_DOCS_TRIGGER_NAME);
-		if (od_trigger == NULL)
-		{
-			od_trigger = GSC_TRIGGER (gsc_trigger_customkey_new(comp,
-					OPEN_DOCS_TRIGGER_NAME, 
-					dw_plugin->priv->conf->od_keys));
-			gsc_completion_register_trigger (comp, od_trigger);
-			g_object_unref(od_trigger);
-		}
-		
-		if (gsc_completion_get_provider(comp,GSC_DOCUMENTWORDS_PROVIDER_NAME)==NULL)
-		{
-			/*FIXME: Create a funtion "update_conf" and we must to 
-			 call it here and when the user changes the configuration.
-			*/
-							
-			if (dw_plugin->priv->conf->ac_enabled)
-			{
-				GscDocumentwordsProvider *dw  = gsc_documentwords_provider_new(view);
-				
-				ur_trigger = gsc_completion_get_trigger(comp, GSC_TRIGGER_USERREQUEST_NAME);
-				if (ur_trigger==NULL)
-				{
-					ur_trigger = GSC_TRIGGER(gsc_trigger_userrequest_new (comp));
-					/*TODO Assign the configured keys*/
-					gsc_completion_register_trigger(comp,ur_trigger);
-					g_object_unref(ur_trigger);
-				}
-				gsc_completion_register_provider(comp,GSC_PROVIDER(dw),ur_trigger);
-				
-				ac_trigger = gsc_completion_get_trigger(comp, GSC_TRIGGER_AUTOWORDS_NAME);
-				if (ac_trigger==NULL)
-				{
-					ac_trigger = GSC_TRIGGER (gsc_trigger_autowords_new(comp));
-					gsc_completion_register_trigger(comp,GSC_TRIGGER(ac_trigger));
-					gsc_trigger_autowords_set_delay(GSC_TRIGGER_AUTOWORDS (ac_trigger),dw_plugin->priv->conf->ac_delay);
-					g_object_unref(ac_trigger);
-				}
-				gsc_completion_register_provider(comp,GSC_PROVIDER(dw),ac_trigger);
-				
-				g_object_unref(dw);
-			}
-			
-			
-			if (dw_plugin->priv->conf->open_enabled)
-			{
-				GscGeditopendocProvider* od = gsc_geditopendoc_provider_new(window);
-				gsc_completion_register_provider(comp,GSC_PROVIDER(od),GSC_TRIGGER (od_trigger));
-				g_object_unref(od);
-			}
-			
-			if (dw_plugin->priv->conf->recent_enabled)
-			{
-				GscGeditrecentProvider *grp = gsc_geditrecent_provider_new(window);
-				gsc_completion_register_provider(comp,GSC_PROVIDER(grp),GSC_TRIGGER (od_trigger));
-				g_object_unref(grp);
-			}
-			
-			g_object_set (comp,
-				      "autoselect", dw_plugin->priv->conf->autoselect_enabled,
-				      NULL);
-			
-			gsc_completion_set_active (comp, TRUE);
-		}
-
-	}
 }
 
 /*************** Configuration window ****************/
-
-static gboolean delete_event( GtkWidget *widget,
-								GdkEvent  *event,
-								gpointer   data )
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)data;
-	gtk_widget_hide(dw_plugin->priv->window);
-	return FALSE;
-}
-
-static void
-autocompletion_enabled_toggled(GtkWidget *button,
-				gpointer user_data)
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)user_data;
-	gboolean active =	gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button));
-
-	gconf_client_set_bool(dw_plugin->priv->gconf_cli,
-				GCONF_AUTOCOMPLETION_ENABLED,
-				active,
-				NULL);
-	dw_plugin->priv->conf->ac_enabled = active;
-
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-		GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-		if (comp!=NULL)
-		{
-			GscTriggerAutowords * ac_trigger = GSC_TRIGGER_AUTOWORDS(
-					gsc_completion_get_trigger(comp,GSC_TRIGGER_AUTOWORDS_NAME));
-			if (ac_trigger!=NULL && !active)
-			{
-				gsc_completion_unregister_trigger(comp,GSC_TRIGGER(ac_trigger));
-			}
-		
-			if (ac_trigger==NULL && active)
-			{
-				ac_trigger = gsc_trigger_autowords_new(comp);
-				gsc_completion_register_trigger(comp,GSC_TRIGGER(ac_trigger));
-				g_object_unref(ac_trigger);
-				gsc_trigger_autowords_set_delay(ac_trigger,dw_plugin->priv->conf->ac_delay);
-			}
-		}
-		windows = g_list_next(windows);
-	}
-}
-
-static void
-open_enabled_toggled(GtkWidget *button,
-			gpointer user_data)
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)user_data;
-	gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button));
-
-	gconf_client_set_bool(dw_plugin->priv->gconf_cli,
-				GCONF_OPEN_ENABLED,
-				active,
-				NULL);
-	
-	dw_plugin->priv->conf->open_enabled = active;
-
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-		GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-		if (comp!=NULL)
-		{
-			GscGeditopendocProvider * prov = GSC_GEDITOPENDOC_PROVIDER(
-					gsc_completion_get_provider(comp,GSC_GEDITOPENDOC_PROVIDER_NAME));
-			
-			GscTrigger *od_trigger = gsc_completion_get_trigger (comp, OPEN_DOCS_TRIGGER_NAME);
-			if (od_trigger != NULL){
-				if (prov!=NULL && !active)
-				{
-					gsc_completion_unregister_provider(comp,GSC_PROVIDER(prov),od_trigger);
-				}
-			
-				if (prov==NULL && active)
-				{
-					prov = gsc_geditopendoc_provider_new(dw_plugin->priv->gedit_window);
-					gsc_completion_register_provider(comp,GSC_PROVIDER(prov),od_trigger);
-					g_object_unref(prov);
-				}
-			}
-		}
-		windows = g_list_next(windows);
-	}
-}
-
-static void
-recent_enabled_toggled(GtkWidget *button,
-			gpointer user_data)
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)user_data;
-	gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button));
-
-	gconf_client_set_bool(dw_plugin->priv->gconf_cli,
-				GCONF_RECENT_ENABLED,
-				active,
-				NULL);
-	
-	dw_plugin->priv->conf->recent_enabled = active;
-
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-		GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-		if (comp!=NULL)
-		{
-			GscGeditrecentProvider * prov = GSC_GEDITRECENT_PROVIDER(
-					gsc_completion_get_provider(comp,GSC_GEDITRECENT_PROVIDER_NAME));
-			GscTrigger *od_trigger = gsc_completion_get_trigger (comp, OPEN_DOCS_TRIGGER_NAME);
-			if (od_trigger != NULL){
-				if (prov!=NULL && !active)
-				{
-					gsc_completion_unregister_provider(comp,GSC_PROVIDER(prov), od_trigger);
-				}
-			
-				if (prov==NULL && active)
-				{
-					prov = gsc_geditrecent_provider_new(dw_plugin->priv->gedit_window);
-					gsc_completion_register_provider(comp,GSC_PROVIDER(prov), od_trigger);
-					g_object_unref(prov);
-				}
-			}
-		}
-		windows = g_list_next(windows);
-	}
-}
-
-static void
-autoselect_enabled_toggled(GtkWidget *button,
-			   gpointer user_data)
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)user_data;
-	gboolean active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button));
-
-	gconf_client_set_bool(dw_plugin->priv->gconf_cli,
-				GCONF_AUTOSELECT_ENABLED,
-				active,
-				NULL);
-	
-	dw_plugin->priv->conf->autoselect_enabled = active;
-
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-		GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-		if (comp!=NULL)
-		{
-			g_object_set (comp,
-				      "autoselect", active,
-				      NULL);
-		}
-		windows = g_list_next(windows);
-	}
-}
-
-static void
-apply_configuration(GtkWidget *button,
-								gpointer user_data)
-{
-	/*Save configuration and exit*/
-	delete_event(button,NULL,user_data);
-}
-
-static gboolean
-delay_changed(GtkWidget *widget,
-								gpointer user_data)
-{
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)user_data;
-	guint delay = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(widget));
-	gconf_client_set_int(dw_plugin->priv->gconf_cli,
-				GCONF_AUTOCOMPLETION_DELAY,
-				delay,
-				NULL);
-	dw_plugin->priv->conf->ac_delay = delay;
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-			GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-			if (comp!=NULL)
-			{
-				GscTriggerAutowords *ac_trigger = GSC_TRIGGER_AUTOWORDS(
-						gsc_completion_get_trigger(comp,GSC_TRIGGER_AUTOWORDS_NAME));
-				if (ac_trigger!=NULL)
-				{
-					gsc_trigger_autowords_set_delay(ac_trigger,dw_plugin->priv->conf->ac_delay);
-				}
-			}
-			windows = g_list_next(windows);
-	}
-	return FALSE;
-}
-
-static void
-_refresh_keys(DocwordscompletionPlugin *dw_plugin)
-{
-	GList *windows = gedit_window_get_views(dw_plugin->priv->gedit_window);
-	while (windows!=NULL)
-	{
-			GscCompletion *comp = gsc_completion_get_from_view(GTK_TEXT_VIEW(windows->data));
-			if (comp!=NULL)
-			{
-				GscTrigger *ur_trigger = 
-						gsc_completion_get_trigger(comp,GSC_TRIGGER_USERREQUEST_NAME);
-
-				if (ur_trigger!=NULL && GSC_IS_TRIGGER_CUSTOMKEY(ur_trigger))
-					gsc_trigger_customkey_set_keys(GSC_TRIGGER_CUSTOMKEY(ur_trigger),
-						dw_plugin->priv->conf->ure_keys);
-					
-				GscTriggerCustomkey *od_trigger = GSC_TRIGGER_CUSTOMKEY(
-						gsc_completion_get_trigger(comp,OPEN_DOCS_TRIGGER_NAME));
-				if (od_trigger!=NULL)
-					gsc_trigger_customkey_set_keys(od_trigger,
-						dw_plugin->priv->conf->od_keys);
-				
-				GValue value = {0,};
-				g_value_init(&value,G_TYPE_STRING);
-				g_value_set_string(&value,dw_plugin->priv->conf->si_keys);
-				g_object_set_property(G_OBJECT(comp),
-							"info-keys",
-							&value);
-				
-					
-			}
-			windows = g_list_next(windows);
-	}
-}
-
-static gboolean
-ure_keys_key_release_cb (GtkWidget *view,
-			GdkEventKey *event, 
-			gpointer user_data)
-{
-	DocwordscompletionPlugin *dw_plugin = (DocwordscompletionPlugin*)user_data;
-	
-	gchar* teclas = gtk_accelerator_name(event->keyval,event->state);
-	gtk_entry_set_text(GTK_ENTRY(view),teclas);
-	gconf_client_set_string(dw_plugin->priv->gconf_cli,
-				GCONF_USER_REQUEST_EVENT_KEYS,
-				teclas,
-				NULL);
-	dw_plugin->priv->conf->ure_keys = teclas;
-	
-	_refresh_keys(dw_plugin);
-	
-	return TRUE;
-}
-
-static gboolean
-od_keys_key_release_cb (GtkWidget *view,
-			GdkEventKey *event, 
-			gpointer user_data)
-{
-	DocwordscompletionPlugin *dw_plugin = (DocwordscompletionPlugin*)user_data;
-	
-	gchar* teclas = gtk_accelerator_name(event->keyval,event->state);
-	gtk_entry_set_text(GTK_ENTRY(view),teclas);
-	gconf_client_set_string(dw_plugin->priv->gconf_cli,
-				GCONF_OPEN_DOCUMENTS_EVENT_KEYS,
-				teclas,
-				NULL);
-	dw_plugin->priv->conf->od_keys = teclas;
-	
-	_refresh_keys(dw_plugin);
-	
-	return TRUE;
-}
-
-static gboolean
-si_keys_key_release_cb (GtkWidget *view,
-			GdkEventKey *event, 
-			gpointer user_data)
-{
-	DocwordscompletionPlugin *dw_plugin = (DocwordscompletionPlugin*)user_data;
-	
-	gchar* teclas = gtk_accelerator_name(event->keyval,event->state);
-	gtk_entry_set_text(GTK_ENTRY(view),teclas);
-	gconf_client_set_string(dw_plugin->priv->gconf_cli,
-				GCONF_SHOW_INFO_KEYS,
-				teclas,
-				NULL);
-	dw_plugin->priv->conf->si_keys = teclas;
-	
-	_refresh_keys(dw_plugin);
-	
-	return TRUE;
-}
-
-static GtkWidget*
-create_configure_dialog(GeditPlugin *plugin)
-{
-	
-	DocwordscompletionPlugin * dw_plugin = (DocwordscompletionPlugin*)plugin;
-	dw_plugin->priv->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	g_signal_connect (G_OBJECT (dw_plugin->priv->window), "delete_event",
-			G_CALLBACK (delete_event), dw_plugin);
-
-	GtkWidget *table = gtk_table_new (8, 2, TRUE);
-	gtk_container_add (GTK_CONTAINER (dw_plugin->priv->window), table);
-	
-	GtkWidget *check_auto = gtk_check_button_new_with_label("Enable autocompletion");
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check_auto),dw_plugin->priv->conf->ac_enabled);
-	gtk_table_attach_defaults (GTK_TABLE (table), check_auto, 0, 2, 0, 1);
-	gtk_widget_show (check_auto);
-	g_signal_connect (G_OBJECT (check_auto), "toggled",
-			G_CALLBACK (autocompletion_enabled_toggled), dw_plugin);
-
-	GtkWidget *label = gtk_label_new("Autocompletion delay: ");
-	gtk_table_attach_defaults (GTK_TABLE (table), label, 0, 1, 1, 2);
-	gtk_widget_show (label);
-	
-	/* creates the spinner, with no decimal places */
-	GtkAdjustment *spinner_adj;
-	spinner_adj = (GtkAdjustment *) gtk_adjustment_new (50.0, 100.0, 2000.0, 50.0, 100.0, 5.0);
-
-	GtkWidget *delay = gtk_spin_button_new (spinner_adj, 1.0, 0);
-	gtk_spin_button_set_value(GTK_SPIN_BUTTON(delay),dw_plugin->priv->conf->ac_delay);
-	gtk_table_attach_defaults (GTK_TABLE (table), delay, 1, 2, 1, 2);
-	gtk_widget_show (delay);
-	
-	g_signal_connect (G_OBJECT (delay), "value-changed",
-			G_CALLBACK (delay_changed), dw_plugin);
-									
-	GtkWidget *label_ure = gtk_label_new("User Request trigger keys: ");
-	gtk_table_attach_defaults (GTK_TABLE (table), label_ure, 0, 1, 2, 3);
-	gtk_widget_show (label_ure);
-	
-	GtkWidget *ure_keys = gtk_entry_new ();
-	gtk_entry_set_text(GTK_ENTRY(ure_keys),dw_plugin->priv->conf->ure_keys);
-	gtk_table_attach_defaults (GTK_TABLE (table), ure_keys, 1, 2, 2, 3);
-	gtk_widget_show (ure_keys);
-	
-	g_signal_connect (G_OBJECT (ure_keys), "key-press-event",
-			G_CALLBACK (ure_keys_key_release_cb), dw_plugin);
-			
-	GtkWidget *check_open = gtk_check_button_new_with_label("Enable Open Documents");
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check_open),dw_plugin->priv->conf->open_enabled);
-	gtk_table_attach_defaults (GTK_TABLE (table), check_open, 0, 2, 3, 4);
-	gtk_widget_show (check_open);
-	g_signal_connect (G_OBJECT (check_open), "toggled",
-			G_CALLBACK (open_enabled_toggled), dw_plugin);
-	
-	GtkWidget *check_recent = gtk_check_button_new_with_label("Enable Recent Documents");
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check_recent),dw_plugin->priv->conf->recent_enabled);
-	gtk_table_attach_defaults (GTK_TABLE (table), check_recent, 0, 2, 4, 5);
-	gtk_widget_show (check_recent);
-	g_signal_connect (G_OBJECT (check_recent), "toggled",
-			G_CALLBACK (recent_enabled_toggled), dw_plugin);
-	
-	
-	GtkWidget *label_od = gtk_label_new("Open/Recent Documents trigger keys: ");
-	gtk_table_attach_defaults (GTK_TABLE (table), label_od, 0, 1, 5, 6);
-	gtk_widget_show (label_od);
-	
-	GtkWidget *od_keys = gtk_entry_new ();
-	gtk_entry_set_text(GTK_ENTRY(od_keys),dw_plugin->priv->conf->od_keys);
-	gtk_table_attach_defaults (GTK_TABLE (table), od_keys, 1, 2, 5, 6);
-	gtk_widget_show (od_keys);
-	
-	g_signal_connect (G_OBJECT (od_keys), "key-press-event",
-			G_CALLBACK (od_keys_key_release_cb), dw_plugin);
-									
-	GtkWidget *label_si = gtk_label_new("Show Info keys: ");
-	gtk_table_attach_defaults (GTK_TABLE (table), label_si, 0, 1, 6, 7);
-	gtk_widget_show (label_si);
-	
-	GtkWidget *si_keys = gtk_entry_new ();
-	gtk_entry_set_text(GTK_ENTRY(si_keys),dw_plugin->priv->conf->si_keys);
-	gtk_table_attach_defaults (GTK_TABLE (table), si_keys, 1, 2, 6, 7);
-	gtk_widget_show (si_keys);
-	
-	g_signal_connect (G_OBJECT (si_keys), "key-press-event",
-			G_CALLBACK (si_keys_key_release_cb), dw_plugin);
-
-	GtkWidget *check_autoselect = gtk_check_button_new_with_label("Enable autoselect proposal");
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check_autoselect),dw_plugin->priv->conf->autoselect_enabled);
-	gtk_table_attach_defaults (GTK_TABLE (table), check_autoselect, 0, 2, 7, 8);
-	gtk_widget_show (check_autoselect);
-	g_signal_connect (G_OBJECT (check_autoselect), "toggled",
-			G_CALLBACK (autoselect_enabled_toggled), dw_plugin);
-	
-	
-	GtkWidget *close = gtk_button_new_from_stock(GTK_STOCK_APPLY);
-	gtk_table_attach_defaults (GTK_TABLE (table), close, 1, 2, 8, 9);
-	gtk_widget_show (close);
-	g_signal_connect (G_OBJECT (close), "clicked",
-			G_CALLBACK (apply_configuration), dw_plugin);
-	
-	gtk_widget_show (table);
-
-	return dw_plugin->priv->window;
-}
 
 static void
 docwordscompletion_plugin_class_init (DocwordscompletionPluginClass *klass)
@@ -685,7 +221,6 @@ docwordscompletion_plugin_class_init (DocwordscompletionPluginClass *klass)
 	plugin_class->activate = impl_activate;
 	plugin_class->deactivate = impl_deactivate;
 	plugin_class->update_ui = impl_update_ui;
-	plugin_class->create_configure_dialog = create_configure_dialog;
 
 	g_type_class_add_private (object_class, 
 				  sizeof (DocwordscompletionPluginPrivate));
